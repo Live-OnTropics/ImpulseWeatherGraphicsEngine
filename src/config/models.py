38ba1d3d4342b2939ec896770f70257b0f127adf
Live-OnTropics <73486481+Live-OnTropics@users.noreply.github.com>
2026-06-05 14:51:10 +0300

    Add model endpoints configuration in models.py

---
+++ src/config/models.py
@@ -0,0 +1,32 @@
+# src/config/models.py
+
+MODEL_ENDPOINTS = [
+    {
+        "name": "NDFD",
+        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/NDFD/NWS/CONUS/CONDUIT/Best",
+        "highs_candidates": ["maximum_temperature_height_above_ground_Mixed_intervals_Maximum", "maximum_temperature_height_above_ground"],
+        "lows_candidates": ["minimum_temperature_height_above_ground_Mixed_intervals_Minimum", "minimum_temperature_height_above_ground"],
+        "max_days": 7
+    },
+    {
+        "name": "HRRR (2.5km)",
+        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/HRRR/CONUS_2p5km/Best",
+        "highs_candidates": ["temperature_height_above_ground"],
+        "lows_candidates": ["temperature_height_above_ground"],
+        "max_days": 2
+    },
+    {
+        "name": "NAM (12km)",
+        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/NAM/CONUS_12km/Best",
+        "highs_candidates": ["temperature_height_above_ground"],
+        "lows_candidates": ["temperature_height_above_ground"],
+        "max_days": 4
+    },
+    {
+        "name": "GFS",
+        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/GFS/Global_0p25deg/Best",
+        "highs_candidates": ["temperature_height_above_ground"],
+        "lows_candidates": ["temperature_height_above_ground"],
+        "max_days": 16
+    }
+]
