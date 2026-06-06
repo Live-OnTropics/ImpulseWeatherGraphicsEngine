13c93d45363988255ec1c6131dca01611e0f0934
Live-OnTropics <73486481+Live-OnTropics@users.noreply.github.com>
2026-06-06 18:00:33 +0300

    Refactor precipitation candidates in models.py
    Removed unnecessary precipitation candidates from various models and updated the list for HRRR.

--- src/config/models.py
+++ src/config/models.py
@@ -6,7 +6,6 @@ MODEL_ENDPOINTS = [
         "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/NDFD/NWS/CONUS/CONDUIT/Best",
         "highs_candidates": ["maximum_temperature_height_above_ground_Mixed_intervals_Maximum", "maximum_temperature_height_above_ground"],
         "lows_candidates": ["minimum_temperature_height_above_ground_Mixed_intervals_Minimum", "minimum_temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
         "max_days": 7
     },
     {
@@ -14,7 +13,7 @@ MODEL_ENDPOINTS = [
         "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/HRRR/CONUS_2p5km/Best",
         "highs_candidates": ["temperature_height_above_ground"],
         "lows_candidates": ["temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_1_Hour_Accumulation", "Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
+        "precip_candidates": ["Total_precipitation_surface_1_Hour_Accumulation"],
         "max_days": 2
     },
     {
@@ -22,31 +21,13 @@ MODEL_ENDPOINTS = [
         "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/NAM/CONUS_12km/Best",
         "highs_candidates": ["temperature_height_above_ground"],
         "lows_candidates": ["temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
         "max_days": 4
-    },
-    {
-        "name": "NAM (3km Nest)",
-        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/NAM/CONUS_3km/Best",
-        "highs_candidates": ["temperature_height_above_ground"],
-        "lows_candidates": ["temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_1_Hour_Accumulation", "Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
-        "max_days": 2
-    },
-    {
-        "name": "RAP (13km)",
-        "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/RAP/CONUS_13km/Best",
-        "highs_candidates": ["temperature_height_above_ground"],
-        "lows_candidates": ["temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_1_Hour_Accumulation", "Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
-        "max_days": 1
     },
     {
         "name": "GFS",
         "url": "https://thredds.ucar.edu/thredds/dodsC/grib/NCEP/GFS/Global_0p25deg/Best",
         "highs_candidates": ["temperature_height_above_ground"],
         "lows_candidates": ["temperature_height_above_ground"],
-        "precip_candidates": ["Total_precipitation_surface_Mixed_intervals_Accumulation", "Total_precipitation_surface_Mixed_intervals", "Total_precipitation_surface"],
         "max_days": 16
     }
 ]
